e1bca64256e7d2f010b47ace64b3ca6d7a88b217
Bweezy1 <118490510+Bweezy1@users.noreply.github.com>
2023-11-26 22:55:17 -0600

    Update (Version 1) minishell.h
    Big block comment added at the top for documentation purposes.

--- (Version 1) minishell.h
+++ (Version 1) minishell.h
@@ -1,3 +1,16 @@
+/*
+Authors: Brandon Nguyen, Tien Nguyen, Peter Phuc Phan
+Course: CPSC 4317
+Assignment: Simple Shell Project
+Instructor: Bo Sun
+
+This is a header program file which contains the interface of the program. 
+This interface is responsible providing a backbone for a .c file (Version 1) minishell.c to work with. (Access to any functions and global variables present)
+When the shell first starts, it will put out "minishell> " to indicate that it is online and ready to take input commands from the user.
+Whether or not the shell execute a command or put out a "command not found" message after reading an input command depends on if the command is valid or not.
+Should the command be valid, the shell will create a child process to execute the command using fork().
+*/
+
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
